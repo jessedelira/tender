dc9a609cc345bafce640861540b3609c9ab135b9
Conner Duling <conner.m.duling@gmail.com>
2021-10-13 17:13:45 -0400

    chicken

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,8 @@ const Header = () => {
         <div className="App">
         <Navbar bg="dark" variant="dark" sticky="dark">
             <Navbar.Brand>
-                <img src="https://cdn-icons.flaticon.com/png/512/2936/premium/2936419.png?token=exp=1634158927~hmac=97396b172d9953dc4096f133337a7777" width="40px" height="40px" alt="chicken"></img>
+                <img src="chicken.png" width="40px" height="40px" id="chicken-logo" alt="chicken logo"/>
+                <label htmlFor="chicken">Tender</label>
             </Navbar.Brand>
         </Navbar>
         </div>
