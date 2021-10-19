51548e9e84de3f5587e93d6fad1de2d3ba74d625
ConnerDuling <conner.m.duling@gmail.com>
2021-10-18 20:30:36 -0400

    Added MapContainer with GoogleAPI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from '../src/components/Header/Header.jsx';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
+import MapContainer from '../src/components/MapContainer/MapContainer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Button>Hi</Button>
+      <MapContainer></MapContainer>
     </div>
   );
 }
