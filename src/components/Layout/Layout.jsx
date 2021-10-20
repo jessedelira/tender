9c11acedc3c920b39c6edb0e19f7666da9147ba8
Jesse De Lira <jessedelira2000@gmail.com>
2021-10-19 22:44:29 -0400

    added routing

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+
+
+const Layout = (props) =>{
+  return(
+    <div>
+        <Container>
+          {props.children}
+        </Container>
+      </div>
+  )
+}
+
+export default Layout;
