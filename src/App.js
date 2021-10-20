617363c68690b91ad0719bd654d1b64e1d6e2cb5
ConnerDuling <conner.m.duling@gmail.com>
2021-10-19 21:56:35 -0400

    Added ChickenCarousel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from '../src/components/Header/Header.jsx';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
-import MapContainer from '../src/components/MapContainer/MapContainer.jsx';
+// import MapContainer from '../src/components/MapContainer/MapContainer.jsx';
+import ChickenCarousel from './components/ChickenCarousel/ChickenCarousel.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Button>Hi</Button>
-      <MapContainer></MapContainer>
+      {/* <MapContainer></MapContainer> */}
+      <ChickenCarousel></ChickenCarousel>
     </div>
   );
 }
