6ab5791da893dad75f94cb9ee33c63cf16f4d323
Conner Duling <conner.m.duling@gmail.com>
2021-10-27 14:10:59 -0400

    Google API for restaurant data

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -1,8 +1,19 @@
 var axios = require('axios');
 
+var location = '-33.8670522%2C151.1957362'
+var radius = '1500'
+var type = 'restaurant'
+var keyword = 'food'
+var key = 'AIzaSyBes962d9aX3zEnafAdIfQsZ43TOZQNClo'
+
 var config = {
   method: 'get',
-  url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBes962d9aX3zEnafAdIfQsZ43TOZQNClo',
+  url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+location
+  +'&radius='+radius
+//   type != keyword, as this can mess with the API call
+  +'&type='+type
+  +'&keyword='+keyword
+  +'&key='+key,
   headers: { }
 };
 
