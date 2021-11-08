801d1ffefad85a464f582294f6a2397881a7bdc0
ConnerDuling <conner.m.duling@gmail.com>
2021-11-07 22:41:09 -0500

    Removed unused route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from '../src/components/Header/Header.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
 import MapContainer from '../src/components/MapContainer/MapContainer.jsx';
-import {Route} from 'react-rou'
 // import MapContainer from '../src/components/MapContainer/MapContainer.jsx';
 import ChickenCarousel from './components/ChickenCarousel/ChickenCarousel.jsx';
 
