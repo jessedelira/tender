fc248d01d14b0c230e8e7b5beffa1e5a47462b49
Jesse De Lira <jessedelira1@gmail.com>
2021-11-16 17:31:35 -0500

    init: backend

---
+++ src/main/java/com/tender/app/TenderApplication.java
@@ -0,0 +1,13 @@
+package com.tender.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderApplication.class, args);
+	}
+
+}
