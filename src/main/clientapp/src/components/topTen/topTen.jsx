1dc3dd7684e72fbf07462580940ee7cf27566d03
ConnerDuling <conner.m.duling@gmail.com>
2021-11-21 12:58:21 -0500

    Top Ten now dynamic

--- src/main/clientapp/src/components/topTen/topTen.jsx
+++ src/main/clientapp/src/components/topTen/topTen.jsx
@@ -6,11 +6,11 @@ const TopTen = () => {
 
     //This needs to be an array of some sort of json elements
     //Need to be key indexed
-    //Likely do a query of the DB getting the top 10 resturaunts based on user ratings.
+    //Likely do a query of the DB getting the top 10 resturaunts based on user scores.
     const myResturaunts = [
-        {key: 1, name: "Umi", rating: 4.2},
-        {key: 2, name: "Don Hall's", rating: 4.1},
-        {key: 3, name: "Firehouse Subs", rating: 4}
+        {key: 1, name: "Umi", score: 4.2},
+        {key: 2, name: "Don Hall's", score: 4.1},
+        {key: 3, name: "Firehouse Subs", score: 4}
       ];
 
     //Returns HTML for each resuraunt
@@ -18,7 +18,7 @@ const TopTen = () => {
         return(
             <section class="1" variant="dark">
                     <h2>{index+1}: {resturaunt.name}</h2>
-                    <h4>Rating: {resturaunt.rating}</h4>
+                    <h4>Score: {resturaunt.score}</h4>
                     <br />
                     <img alt="No Current Image" src="" width="30%" height="30%"/>
             </section>
