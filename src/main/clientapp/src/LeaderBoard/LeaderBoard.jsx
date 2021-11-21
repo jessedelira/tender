d37d308a8f52a2e36564d2a3b592defa1a444061
ConnerDuling <conner.m.duling@gmail.com>
2021-11-21 13:11:03 -0500

    Leaderboard is now dynamic

--- src/main/clientapp/src/LeaderBoard/LeaderBoard.jsx
+++ src/main/clientapp/src/LeaderBoard/LeaderBoard.jsx
@@ -1,76 +1,42 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 const LeaderBoard = () => {
-    return (
-        
-        <Table striped bordered hover variant="dark">
-  <thead>
-      
-    <tr>
-      <th>#</th>
-      <th>Restaurant</th>
-      <th>Type</th>
-      <th>Score</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Cluckin' Bell</td>
-      <td>Fried Chicken / Mexican</td>
-      <td>10.0</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>KFC</td>
-      <td>Fried Chicken</td>
-      <td>9.6</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Krusty Krab</td>
-      <td>Seafood? / Pizza</td>
-      <td>9.2</td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>Pizza Planet</td>
-      <td>Pizza</td>
-      <td>8.5</td>
-    </tr>
-    <tr>
-      <td>5</td>
-      <td>Whataburger</td>
-      <td>burger mhmm</td>
-      <td>7.21</td>
-    </tr>
-    <tr>
-      <td>.</td>
-      <td>.</td>
-      <td>.</td>
-      <td>.</td>
-    </tr>
-    <tr>
-      <td>..</td>
-      <td>..</td>
-      <td>..</td>
-      <td>..</td>
-    </tr>
-    <tr>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-    </tr>
-    <tr>
-      <td>9999</td>
-      <td>Chum Bucket</td>
-      <td>Chum</td>
-      <td>0.01</td>
-    </tr>
+    
+  //Array of JSON elements. Need to query the DB here.
+  const myResturaunts = [
+        {key: 1, name: "Umi", score: 4.2},
+        {key: 2, name: "Don Hall's", score: 4.1},
+        {key: 3, name: "Firehouse Subs", score: 4}
+      ];
 
-  </tbody>
-</Table>
+
+  //Returns HTML for each row
+  const renderLeaderboard = (resturaunt, index) => {
+    return(
+          <tr>
+            <td>{resturaunt.key}</td>
+            <td>{resturaunt.name}</td>
+            <td>N/A</td>
+            <td>{resturaunt.score}</td>
+          </tr>
+    )
+  }
+
+  return (
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Restaurant</th>
+            <th>Type</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {myResturaunts.map(renderLeaderboard)}
+        </tbody>
+      </Table>
     )
 }
 
