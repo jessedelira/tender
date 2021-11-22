ea64af824b9610e2f319e13f52a74b348d6ec488
Noel <barrnj01@pfw.edu>
2021-11-22 17:09:39 -0500

    updated profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,71 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Background from './Background';
+import './Profile.css';
 function Profile() {
     return (
         <div className="container">
-            <Background></Background>
+            <div className="topProfile">
+                <div className="name">
+                    <img id="pfp" src="beach.png" alt="" />
+                </div>
+                <div>
+                    <h1>Dan Filler</h1>
+                </div>
+                <h5>Reviews</h5>
+            </div>
+            <div className="review-container">
+                <div className="row">
+                    <div className="review">
+                        <div className="square-container">
+                            <div className="square"></div>
+                            <div className="square"></div>
+                            <div className="square"></div>
+                            <div className="square"></div>
+                            <div className="square"></div>
+                        </div>
+                        <div>
+                        <h1>Resturant</h1>
+                        <h2>Title</h2>
+                        </div>
+                        <div id="review">
+                            review content
+                        </div>
+                        <div>
+                            User
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="review">
+                        <div>
+                            <div className="square"></div>
+                        </div>
+                        <h1>Resturant</h1>
+                        <h2>Title</h2>
+                        <div>
+                            review content
+                        </div>
+                        <div>
+                            User
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="review">
+                        <div>
+                            <div className="square"></div>
+                        </div>
+                        <h1>Resturant</h1>
+                        <h2>Title</h2>
+                        <div>
+                            review content
+                        </div>
+                        <div>
+                            User
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
