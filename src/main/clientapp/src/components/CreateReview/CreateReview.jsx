f02a44886537fb219b48f8d002dbeb6db22a1852
Jesse De Lira <jessedelira1@gmail.com>
2021-12-06 23:07:16 -0500

    added export for createReview

--- src/main/clientapp/src/components/CreateReview/CreateReview.jsx
+++ src/main/clientapp/src/components/CreateReview/CreateReview.jsx
@@ -44,8 +44,4 @@ const CreateReview = () => {
     )
 }
 
-<<<<<<< HEAD
-export default CreateReview
-=======
-export default CreateReview
->>>>>>> c0219b47e8f2121032e00eb86751aeee2b7d3c3a
+export default CreateReview;
